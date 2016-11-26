5b32b3cd5cd197fb0762f5cf329d16abb8f37b5d
simon <ihmig@kaliber5.de>
2016-11-27 00:44:04 +0100

    setup for ember canary

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,2 +1,12 @@
 module.exports = {
+  scenarios: [
+    {
+      name: 'default',
+      bower: {
+        dependencies: {} /* No dependencies needed as the
+         default is already specified in
+         the consuming app's bower.json */
+      }
+    }
+  ]
 };
